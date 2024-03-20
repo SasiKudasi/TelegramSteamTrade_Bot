ed66040f4a686c9ea87d870c03b996ffa9a2dd96
SasiKudasi <sasha.mittal@yandex.ru>
2024-03-20 16:00:27 +0300

    Добавлен класс SteamMethod, который содердит асинхронный метод для поиска цены на предмет

--- Program.cs
+++ Program.cs
@@ -2,49 +2,16 @@
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
+using TelegramSteamTrade_Bot;
 
 class Program
 {
     static async Task Main(string[] args)
     {
-        string itemName = "AK-47 | Redline (Minimal Wear)"; // Название предмета
-      
-        string url = $"https://steamcommunity.com/market/priceoverview/?appid=730&currency=5&market_hash_name={Uri.EscapeDataString(itemName)}";
-        double res = 0.0;
-        
-        using (var httpClient = new HttpClient())
-        {            
-            try 
-            {
-                
-                HttpResponseMessage response = await httpClient.GetAsync(url);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    string responseBody = await response.Content.ReadAsStringAsync();
-                    var jsonResponse = JsonSerializer.Deserialize<GetRequestFields>(responseBody);
-                    if (jsonResponse != null)
-                    {
-                        var lowestPrice = jsonResponse.lowest_price.ToString();
-                        bool success = double.TryParse(lowestPrice.AsSpan(0, lowestPrice.IndexOf(" ")), out res);
-                        Console.WriteLine($"Response Content: item name {itemName} : {res} руб.");
-                    }
-                    
-                }
-                else
-                {
-                    Console.WriteLine($"Такого предмета не существует");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error: {ex.Message}");
-            }
-        }
+        SteamMethod steam = new();
+        await steam.SearchItemPriceAsync();
         Console.ReadKey();
+       
     }
 }
-public class GetRequestFields
-{
-    public string lowest_price { get; set; }
-}
+
---
+++ SteamMethod.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TelegramSteamTrade_Bot
+{
+    public class SteamMethod
+    {
+
+        public async Task SearchItemPriceAsync(int gameId, string itemName)
+        {           
+            string url = $"https://steamcommunity.com/market/priceoverview/?appid={gameId}&currency=5&market_hash_name={Uri.EscapeDataString(itemName)}";
+            double res = 0.0;
+
+            using (var httpClient = new HttpClient())
+            {
+                try
+                {
+
+                    HttpResponseMessage response = await httpClient.GetAsync(url);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        var jsonResponse = JsonSerializer.Deserialize<GetRequestFields>(responseBody);
+                        if (jsonResponse != null)
+                        {
+                            var lowestPrice = jsonResponse.lowest_price.ToString();
+                            bool success = double.TryParse(lowestPrice.AsSpan(0, lowestPrice.IndexOf(" ")), out res);
+                            Console.WriteLine($"Response Content: item name {itemName} : {res} руб.");
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Такого предмета не существует");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        }
+    }
+    public class GetRequestFields
+    {
+        public string lowest_price { get; set; }
+    }
+}
